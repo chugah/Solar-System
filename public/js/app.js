3de3821f9feeaa53cb1a5017cdc825cc576f94cf
Sami Barghshoon <chugah@users.noreply.github.com>
2016-12-07 12:26:42 -0500

    Added more planets and animation

--- public/js/app.js
+++ public/js/app.js
@@ -15,10 +15,11 @@ BjsApp.init = function(){
 
 	//create camera
 	var camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 15, BABYLON.Vector3.Zero(), scene);
+	// var camera = new BABYLON.TouchCamera('camera', BABYLON.Vector3.Zero(), scene); applies to mobile devices
 
 	//Let user move the camera
 	camera.attachControl(canvas);
-	camera.upperRadiusLimit = 100;
+	camera.upperRadiusLimit = 100; // not needed for TouchCamera when used on mobile devices
 	
 	// light
 	var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
@@ -45,6 +46,29 @@ BjsApp.init = function(){
 	var planet1 = BABYLON.Mesh.CreateSphere('planet1', 16, 1, scene);
 	planet1.position.x = 4;
 	planet1.material = planetMaterial;
+	planet1.orbit = {
+		radius: planet1.position.x,
+		speed: 0.01,
+		angle: 0
+	};
+
+	var planet2 = BABYLON.Mesh.CreateSphere('planet2', 16, 1, scene);
+	planet2.position.x = 6;
+	planet2.material = planetMaterial;
+	planet2.orbit = {
+		radius: planet2.position.x,
+		speed: -0.01,
+		angle: 0
+	};
+
+	var planet3 = BABYLON.Mesh.CreateSphere('planet3', 16, 1, scene);
+	planet3.position.x = 8;
+	planet3.material = planetMaterial;
+	planet3.orbit = {
+		radius: planet3.position.x,
+		speed: 0.02,
+		angle: 0
+	};
 
 	// create skybox for background
 	var skybox = BABYLON.Mesh.CreateBox('skybox', 1000, scene);
@@ -65,6 +89,22 @@ BjsApp.init = function(){
 	// texture of each side of background box 
 	skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture('images/skybox', scene);
 	skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+
+	// enable animate or movement of planets
+	scene.beforeRender = function() {
+		planet1.position.x = planet1.orbit.radius * Math.sin(planet1.orbit.angle);
+		planet1.position.z = planet1.orbit.radius * Math.cos(planet1.orbit.angle);
+		planet1.orbit.angle += planet1.orbit.speed;
+
+		planet2.position.x = planet2.orbit.radius * Math.sin(planet2.orbit.angle);
+		planet2.position.z = planet2.orbit.radius * Math.cos(planet2.orbit.angle);
+		planet2.orbit.angle += planet2.orbit.speed;
+
+		planet3.position.x = planet3.orbit.radius * Math.sin(planet3.orbit.angle);
+		planet3.position.z = planet3.orbit.radius * Math.cos(planet3.orbit.angle);
+		planet3.orbit.angle += planet3.orbit.speed;
+	};
+
 
 	// render the scene
 	engine.runRenderLoop(function(){
